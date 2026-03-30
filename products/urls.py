3aec04b6b24f359cca7434e96687d8db78e444ce
Sok Limkhy <soklimkhy@gmail.com>
2026-03-30 12:55:51 +0700

    feat: Configure SimpleRouter to disable trailing slashes for product URLs

--- products/urls.py
+++ products/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 from .views import ProductViewSet
 
-router = SimpleRouter()
+router = SimpleRouter(trailing_slash=False)
 router.register(r'', ProductViewSet, basename='product')
 
 urlpatterns = [
