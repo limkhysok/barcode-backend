4e408e66e741478ea09b858fe920a8f4881ba0d3
Sok Limkhy <soklimkhy@gmail.com>
2026-03-31 21:40:59 +0700

    feat: update settings.py to use environment variable for ALLOWED_HOSTS and add .env.example file

--- core/settings.py
+++ core/settings.py
@@ -36,7 +36,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "True").lower() in ("true", "1", "t")
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
 
 APPEND_SLASH = True
 
